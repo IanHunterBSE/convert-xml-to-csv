5106ef8f6e2c4b315838cb978b60774957229b9b
Andrea Knabe <andrea.knabe@posteo.de>
2015-06-04 01:59:14 +0200

    Update test3.py

--- test3.py
+++ test3.py
@@ -5,11 +5,11 @@ import xml.etree.ElementTree as ET
 import sys
 import codecs
 
-tree = ET.parse('/Users/andreaks/berlin_1306.xml')
+tree = ET.parse('/file.xml')
 elem = tree.getroot()
 
 import sys
-f = codecs.open('/Users/andreaks/berlin_1306.csv', 'w', encoding="utf-8")
+f = codecs.open('/file.csv', 'w', encoding="utf-8")
 sys.stdout = f
 
 for child_of_elem in elem:
